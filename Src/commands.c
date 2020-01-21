35c978b405d88f44f712216c1309dbbbd7d51535
Jan Pomikalek <jan.pomikalek@gmail.com>
2020-01-21 19:44:48 +0100

    COMM_GET_VALUES fixes: high/low cell, BMS temperature

--- Src/commands.c
+++ Src/commands.c
@@ -4,24 +4,17 @@
 #include "buffer.h"
 #include "config.h"
 #include "datatypes.h"
+#include "main.h"
+#include "AuxFunctions.h"
 #include "ADC_LL.h"
 #include "LTC6803_3_DD.h"
 
 #define CELSIUS_TO_KELVIN_1E1 2732  // add 273.2 to Celsius to get Kelvin
 
-//extern runtimeParameters runtimePars;
+extern runtimeParameters runtimePars;
+extern nonVolParameters nonVolPars;
 
 static void(* volatile send_func)(unsigned char *data, unsigned int len) = 0;
-
-// TODO: use nonVolPars.chgParas.packCellCount instead
-uint8_t cell_count() {
-	for (uint8_t i = 0; i < MAX_CELLS; i++) {
-		if (LTC6803_getCellVoltage(i) == 0)
-			return i;
-	}
-
-	return MAX_CELLS;
-}
 
 /**
  * Send a packet using the set send function.
@@ -69,10 +62,12 @@ void commands_process_packet(unsigned char *data, unsigned int len,
 
 		send_buffer[ind++] = 50;  // FIXME: SoC
 
-		buffer_append_float32(send_buffer, 4.0, 1e3, &ind);  // FIXME: cellVoltageHigh
-		buffer_append_float32(send_buffer, 3.9, 1e3, &ind);  // FIXME: cellVoltageAverage
-		buffer_append_float32(send_buffer, 3.8, 1e3, &ind);  // FIXME: cellVoltageLow
-		buffer_append_float32(send_buffer, 0.2, 1e3, &ind);  // FIXME: cellVoltageMisMatch
+		uint16_t cell_high = highestCell(nonVolPars.chgParas.packCellCount);
+		uint16_t cell_low = lowestCell(nonVolPars.chgParas.packCellCount);
+		buffer_append_int32(send_buffer, cell_high, &ind);  // cellVoltageHigh
+		buffer_append_int32(send_buffer, (cell_high + cell_low) / 2, &ind);  // cellVoltageAverage (very rough approximation)
+		buffer_append_int32(send_buffer, cell_low, &ind);  // cellVoltageLow
+		buffer_append_int32(send_buffer, cell_high - cell_low, &ind);  // cellVoltageMisMatch
 
 		buffer_append_float16(send_buffer, 0.0, 1e2, &ind);  // loCurrentLoadVoltage
 		buffer_append_float16(send_buffer, 0.0, 1e2, &ind);  // loCurrentLoadCurrent
@@ -81,11 +76,15 @@ void commands_process_packet(unsigned char *data, unsigned int len,
 		buffer_append_float16(send_buffer, 0.0, 1e2, &ind);  // auxVoltage
 		buffer_append_float16(send_buffer, 0.0, 1e2, &ind);  // auxCurrent
 
-		// TODO: where does ADC_convertedResults[mcuInternalTemp] fit?
 		buffer_append_int16(send_buffer, (int16_t) (ADC_convertedResults[externalTemp] * 1e1 - CELSIUS_TO_KELVIN_1E1), &ind);  // tempBatteryHigh
 		buffer_append_int16(send_buffer, (int16_t) (ADC_convertedResults[externalTemp] * 1e1 - CELSIUS_TO_KELVIN_1E1), &ind);  // tempBatteryAverage
-		buffer_append_int16(send_buffer, (int16_t) (LTC6803_getTemperature() * 1e1 - CELSIUS_TO_KELVIN_1E1), &ind);  // tempBMSHigh
-		buffer_append_int16(send_buffer, (int16_t) (LTC6803_getTemperature() * 1e1 - CELSIUS_TO_KELVIN_1E1), &ind);  // tempBMSAverage
+
+		uint16_t mcu_temp = ADC_convertedResults[mcuInternalTemp];
+		uint16_t ltc_temp = LTC6803_getTemperature();
+		uint16_t bms_temp_high = (mcu_temp > ltc_temp) ? mcu_temp : ltc_temp;
+		uint16_t bms_temp_avg = (mcu_temp + ltc_temp) / 2;
+		buffer_append_int16(send_buffer, (int16_t) (bms_temp_high * 1e1 - CELSIUS_TO_KELVIN_1E1), &ind);  // tempBMSHigh
+		buffer_append_int16(send_buffer, (int16_t) (bms_temp_avg * 1e1 - CELSIUS_TO_KELVIN_1E1), &ind);  // tempBMSAverage
 
 		// TODO: use runtimePars.chargingState and runtimePars.charging
 		send_buffer[ind++] = (uint8_t) OP_STATE_INIT;  // FIXME: operationalState
@@ -100,9 +99,8 @@ void commands_process_packet(unsigned char *data, unsigned int len,
 	case COMM_GET_BMS_CELLS:
 		send_buffer[ind++] = COMM_GET_BMS_CELLS;
 
-		uint8_t cells = cell_count();
-		send_buffer[ind++] = cells;
-		for (int i = 0; i < cells; i++) {
+		send_buffer[ind++] = nonVolPars.chgParas.packCellCount;
+		for (int i = 0; i < nonVolPars.chgParas.packCellCount; i++) {
 			int sign = LTC6803_getCellDischarge(i) ? -1 : 1;
 			buffer_append_int16(send_buffer, (int16_t) (LTC6803_getCellVoltage(i) * sign), &ind);
 		}
