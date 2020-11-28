df61e860cbd35390c4c8dfb3947d620fd37b0764
Simo Sihvonen <simo.sihvonen@gmail.com>
2020-11-28 23:36:42 +0200

    -added CanRxRefreshActive "9" to the commands.c

--- Src/commands.c
+++ Src/commands.c
@@ -147,6 +147,7 @@ void commands_process_packet(unsigned char *data, unsigned int len,
 		nonVolPars.genParas.canActivityTick = data[ind++];
 		nonVolPars.genParas.canID = data[ind++];
 		nonVolPars.genParas.duringActive5vOn = data[ind++];
+		nonVolPars.genParas.canRxRefreshActive = buffer_get_uint16(data, &ind);
 		ind = 0;
 		send_buffer[ind++] = packet_id;
 		send_buffer[ind++] = nonVolPars.genParas.canID;
@@ -189,6 +190,7 @@ void commands_process_packet(unsigned char *data, unsigned int len,
 		send_buffer[ind++] = nonVolPars.genParas.canActivityTick;
 		send_buffer[ind++] = nonVolPars.genParas.canID;
 		send_buffer[ind++] = nonVolPars.genParas.duringActive5vOn;
+		buffer_append_uint16(send_buffer, nonVolPars.genParas.canRxRefreshActive, &ind);
 		commands_send_packet(send_buffer, ind);
 		break;
 	case COMM_STORE_BMS_CONF:
